f6c38bec0b9d879945c31fe9dc5664fd85024044
SabinaValeriia <valeriia.sabina@onix-systems.com>
2023-06-28 19:35:19 +0300

    L4-page-layout-work

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,3 +1,89 @@
 <template lang="pug">
-h1 Команда
+.team
+    .team--block
+        h1 People and teams
+        input(type="search" placeholder="Search for people and teams")
+        h2 You work with
+        .team--people
+            .team--people-block(v-for="people in team")
+                img(:src="require(`../assets/img/${people.img}`)")
+                h3 {{ people.name }}
+                p {{ people.nickName }}
 </template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const team = [
+  {
+    name: "Yurii Kovalenko",
+    nickName: "yurii.kovalenko",
+    img: "people1.png",
+  },
+  {
+    name: "Marina Permakhova",
+    nickName: "marina.permakhova",
+    img: "people2.png",
+  },
+];
+</script>
+
+<style lang="scss">
+.team {
+  width: 100%;
+  &--block {
+    width: 920px;
+    margin: 0 auto;
+    h1 {
+      font-size: 24px;
+      line-height: 28px;
+      color: #172b4d;
+      margin: 24px 0 32px;
+    }
+    input {
+      width: 100%;
+      height: 40px;
+      transition: border-bottom-color 0.25s ease-in-out 0s;
+      border-bottom: 2px solid #b3bac5;
+      border-top: none;
+      border-left: none;
+      border-right: none;
+      padding: 0 5px;
+    }
+  }
+  &--people {
+    display: flex;
+    gap: 8px;
+    &-block {
+      width: 147px;
+      height: 180px;
+      border: 1px solid #d6d9de;
+      box-sizing: border-box;
+      box-shadow: var(
+        --ds-shadow-raised,
+        0 1px 1px rgba(9, 30, 66, 0.25),
+        0 0 1px rgba(9, 30, 66, 0.31)
+      );
+      transition: box-shadow 0.3s ease-in-out 0s, border-color 0.6s ease-in 0s;
+      border-radius: 6px;
+      img {
+        border-radius: 50%;
+        width: 72px;
+        display: block;
+        margin: 20px auto;
+      }
+      p {
+        color: #6b778c;
+        font-size: 11px;
+        line-height: 16px;
+        text-align: center;
+      }
+      h3  {
+        text-align: center;
+        margin: 0;
+        font-size: 14px;
+      }
+    }
+  }
+}
+</style>
--- src/views/YourWork.vue
+++ src/views/YourWork.vue
@@ -1,3 +1,112 @@
 <template lang="pug">
-h1 Your Work
+.container-work 
+    .your-work
+        h1 Ваша робота
+        .block
+            h3 Нещодавні проекти 
+            router-link(:to="{ name: 'projects' }") Всі проєкти
+        .your-work--block
+            .your-work--block-item(v-for="work in works")
+                img(:src="require(`../assets/img/${work.img}`)")
+                .item
+                    h4 {{ work.name }}
+                    p {{ work.desc }}
+                    h5 швидкі посилання 
+                    h6 Мої відкриті задачі
+                    h6 Виконані задачі
+                    h6 {{ work.board}} дoшка
 </template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const works = [
+  {
+    name: "onix-time-manager",
+    desc: "Програмное обеспечение",
+    board: 1,
+    img: "project1.svg",
+  },
+  {
+    name: "Goals",
+    desc: "Бизнес-проект, управляемый компанией",
+    board: 1,
+    img: "project1.svg",
+  },
+  {
+    name: "Onix: Resource Exchange",
+    desc: "Програмное обеспечение",
+    board: 0,
+    img: "project1.svg",
+  },
+  {
+    name: "Pre-Sale II",
+    desc: "Програмное обеспечение",
+    board: 0,
+    img: "project1.svg",
+  },
+];
+</script>
+
+<style lang="scss">
+.container-work {
+  margin: 0 90px;
+}
+.your-work {
+    h1 {
+
+    }
+    .block {
+        display: flex;
+        justify-content: space-between;
+    }
+  &--block {
+    display: flex;
+    gap: 16px;
+    &-item {
+      height: 168px;
+      width: 240px;
+      border-left: 20px solid #ffbdad;
+      border-radius: 3px;
+      display: flex;
+      box-sizing: border-box;
+      box-shadow: var(
+        --ds-shadow-raised,
+        0 1px 1px rgba(9, 30, 66, 0.25),
+        0 0 1px rgba(9, 30, 66, 0.31)
+      );
+      transition: box-shadow 0.3s ease-in-out 0s, border-color 0.6s ease-in 0s;
+      img {
+        height: 24px;
+        margin: 20px 0 0 -11px;
+      }
+      .item {
+        margin: 0 0 0 10px;
+        h4 {
+          margin: 18px 0 0;
+          font-size: 14px;
+        }
+        h5 {
+          font-size: 11px;
+          color: #6b778c;
+          text-transform: uppercase;
+          margin: 15px 0;
+        }
+        h6 {
+          font-size: 11px;
+          color: #172b4d;
+          margin: 10px 0;
+        }
+        p {
+          margin: 0;
+          font-size: 14px;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          width: 200px;
+        }
+      }
+    }
+  }
+}
+</style>
