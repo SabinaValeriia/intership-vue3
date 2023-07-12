00759d59c5c73acf65df5a36188c2b2fdefcecbb
SabinaValeriia <valeriia.sabina@onix-systems.com>
2023-07-12 12:26:50 +0200

    L6 - drag&drop fix

--- src/views/CanbanView.vue
+++ src/views/CanbanView.vue
@@ -6,41 +6,41 @@
       h3 To Do
         span {{ tasks.toDo.length }}
       draggable.list-group(
-          v-model="tasks.toDo",
-          group="tasksGroup",
-          :disabled="!enabled",
-          item-key="name",
-          ghost-class="ghost",
-          :move="checkMove",
-          @start="dragging = true",
-          @end="dragging = false"
-        )
-          template(#item="{ element }")
-            .task-item(:class="{ 'not-draggable': !enabled }") {{ element.name }}
-              .task-block
-                .task-desc
-                  img(
-                    src="https://onix-systems.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10303?size=medium",
-                    alt="Баг"
+        v-model="tasks.toDo",
+        group="tasksGroup",
+        :disabled="!enabled",
+        item-key="name",
+        ghost-class="ghost",
+        :move="checkMove",
+        @start="dragging = true",
+        @end="dragging = false"
+      )
+        template(#item="{ element }")
+          .task-item(:class="{ 'not-draggable': !enabled }") {{ element.name }}
+            .task-block
+              .task-desc
+                img(
+                  src="https://onix-systems.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10303?size=medium",
+                  alt="Баг"
+                )
+                img(
+                  src="https://onix-systems.atlassian.net/images/icons/priorities/medium.svg",
+                  alt="Приоритет Normal"
+                )
+                svg(
+                  width="24",
+                  height="24",
+                  viewBox="0 0 24 24",
+                  role="presentation"
+                )
+                  path(
+                    d="M16 12c0-1.9-1.3-3.4-3-3.9V4c0-.6-.4-1-1-1s-1 .4-1 1v4.1c-1.7.4-3 2-3 3.9s1.3 3.4 3 3.9V20c0 .6.4 1 1 1s1-.4 1-1v-4.1c1.7-.5 3-2 3-3.9zm-4 2c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z",
+                    fill="currentColor",
+                    fill-rule="evenodd"
                   )
-                  img(
-                    src="https://onix-systems.atlassian.net/images/icons/priorities/medium.svg",
-                    alt="Приоритет Normal"
-                  )
-                  svg(
-                    width="24",
-                    height="24",
-                    viewBox="0 0 24 24",
-                    role="presentation"
-                  )
-                    path(
-                      d="M16 12c0-1.9-1.3-3.4-3-3.9V4c0-.6-.4-1-1-1s-1 .4-1 1v4.1c-1.7.4-3 2-3 3.9s1.3 3.4 3 3.9V20c0 .6.4 1 1 1s1-.4 1-1v-4.1c1.7-.5 3-2 3-3.9zm-4 2c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z",
-                      fill="currentColor",
-                      fill-rule="evenodd"
-                    )
-                .task-desc 
-                  h6 {{ $route.params.key }}
-                  img(src="../assets/img/avatar.png")
+              .task-desc 
+                h6 {{ $route.params.key }}
+                img(src="../assets/img/avatar.png")
     .column
       h3 In Progress
         span {{ tasks.inProgress.length }}
@@ -85,80 +85,80 @@
       h3 Review
         span {{ tasks.review.length }}
       draggable.list-group(
-          v-model="tasks.review",
-          group="tasksGroup",
-          :disabled="!enabled",
-          item-key="name",
-          ghost-class="ghost",
-          :move="checkMove",
-          @start="dragging = true",
-          @end="dragging = false"
-        )
-          template(#item="{ element }")
-            .task-item(:class="{ 'not-draggable': !enabled }") {{ element.name }}
-              .task-block
-                .task-desc
-                  img(
-                    src="https://onix-systems.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10303?size=medium",
-                    alt="Баг"
+        v-model="tasks.review",
+        group="tasksGroup",
+        :disabled="!enabled",
+        item-key="name",
+        ghost-class="ghost",
+        :move="checkMove",
+        @start="dragging = true",
+        @end="dragging = false"
+      )
+        template(#item="{ element }")
+          .task-item(:class="{ 'not-draggable': !enabled }") {{ element.name }}
+            .task-block
+              .task-desc
+                img(
+                  src="https://onix-systems.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10303?size=medium",
+                  alt="Баг"
+                )
+                img(
+                  src="https://onix-systems.atlassian.net/images/icons/priorities/medium.svg",
+                  alt="Приоритет Normal"
+                )
+                svg(
+                  width="24",
+                  height="24",
+                  viewBox="0 0 24 24",
+                  role="presentation"
+                )
+                  path(
+                    d="M16 12c0-1.9-1.3-3.4-3-3.9V4c0-.6-.4-1-1-1s-1 .4-1 1v4.1c-1.7.4-3 2-3 3.9s1.3 3.4 3 3.9V20c0 .6.4 1 1 1s1-.4 1-1v-4.1c1.7-.5 3-2 3-3.9zm-4 2c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z",
+                    fill="currentColor",
+                    fill-rule="evenodd"
                   )
-                  img(
-                    src="https://onix-systems.atlassian.net/images/icons/priorities/medium.svg",
-                    alt="Приоритет Normal"
-                  )
-                  svg(
-                    width="24",
-                    height="24",
-                    viewBox="0 0 24 24",
-                    role="presentation"
-                  )
-                    path(
-                      d="M16 12c0-1.9-1.3-3.4-3-3.9V4c0-.6-.4-1-1-1s-1 .4-1 1v4.1c-1.7.4-3 2-3 3.9s1.3 3.4 3 3.9V20c0 .6.4 1 1 1s1-.4 1-1v-4.1c1.7-.5 3-2 3-3.9zm-4 2c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z",
-                      fill="currentColor",
-                      fill-rule="evenodd"
-                    )
-                .task-desc 
-                  h6 {{ $route.params.key }}
-                  img(src="../assets/img/avatar.png")
+              .task-desc 
+                h6 {{ $route.params.key }}
+                img(src="../assets/img/avatar.png")
     .column
       h3 Done
         span {{ tasks.done.length }}
       draggable.list-group(
-          v-model="tasks.done",
-          group="tasksGroup",
-          :disabled="!enabled",
-          item-key="name",
-          ghost-class="ghost",
-          :move="checkMove",
-          @start="dragging = true",
-          @end="dragging = false"
-        )
-          template(#item="{ element }")
-            .task-item(:class="{ 'not-draggable': !enabled }") {{ element.name }}
-              .task-block
-                .task-desc
-                  img(
-                    src="https://onix-systems.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10303?size=medium",
-                    alt="Баг"
+        v-model="tasks.done",
+        group="tasksGroup",
+        :disabled="!enabled",
+        item-key="name",
+        ghost-class="ghost",
+        :move="checkMove",
+        @start="dragging = true",
+        @end="dragging = false"
+      )
+        template(#item="{ element }")
+          .task-item(:class="{ 'not-draggable': !enabled }") {{ element.name }}
+            .task-block
+              .task-desc
+                img(
+                  src="https://onix-systems.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10303?size=medium",
+                  alt="Баг"
+                )
+                img(
+                  src="https://onix-systems.atlassian.net/images/icons/priorities/medium.svg",
+                  alt="Приоритет Normal"
+                )
+                svg(
+                  width="24",
+                  height="24",
+                  viewBox="0 0 24 24",
+                  role="presentation"
+                )
+                  path(
+                    d="M16 12c0-1.9-1.3-3.4-3-3.9V4c0-.6-.4-1-1-1s-1 .4-1 1v4.1c-1.7.4-3 2-3 3.9s1.3 3.4 3 3.9V20c0 .6.4 1 1 1s1-.4 1-1v-4.1c1.7-.5 3-2 3-3.9zm-4 2c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z",
+                    fill="currentColor",
+                    fill-rule="evenodd"
                   )
-                  img(
-                    src="https://onix-systems.atlassian.net/images/icons/priorities/medium.svg",
-                    alt="Приоритет Normal"
-                  )
-                  svg(
-                    width="24",
-                    height="24",
-                    viewBox="0 0 24 24",
-                    role="presentation"
-                  )
-                    path(
-                      d="M16 12c0-1.9-1.3-3.4-3-3.9V4c0-.6-.4-1-1-1s-1 .4-1 1v4.1c-1.7.4-3 2-3 3.9s1.3 3.4 3 3.9V20c0 .6.4 1 1 1s1-.4 1-1v-4.1c1.7-.5 3-2 3-3.9zm-4 2c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z",
-                      fill="currentColor",
-                      fill-rule="evenodd"
-                    )
-                .task-desc 
-                  h6 {{ $route.params.key }}
-                  img(src="../assets/img/avatar.png")
+              .task-desc 
+                h6 {{ $route.params.key }}
+                img(src="../assets/img/avatar.png")
 </template>
 
 <script lang="ts" setup>
@@ -190,7 +190,9 @@ const dragging = ref(false);
   padding: 20px 5px;
   border-radius: 6px;
 }
-
+.list-group {
+  height: 100%;
+}
 h3 {
   margin: 0 0 10px 10px;
   color: #42526e;
--- src/views/ProjectsItems.vue
+++ src/views/ProjectsItems.vue
@@ -11,7 +11,6 @@
 </template>
 
 <script setup lang="ts">
-import TestComp from "../components/TestComp.vue";
 
 import { inject } from "vue";
 
